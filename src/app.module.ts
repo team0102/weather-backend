4550e7ff21ac1927fc822b6ef4d186f177039908
KJ.Park <rudwos6@naver.com>
2024-02-01 15:23:35 +0900

    🌈 feat: 유저 팔로우 삭제 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './config/typeORM.config';
 import { ConfigModule } from '@nestjs/config';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
-import { UserModule } from './user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+
+import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
