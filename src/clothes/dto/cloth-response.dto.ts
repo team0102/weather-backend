04b29403a1f6473c2aa77997ffacb8057e074367
imjeongjin <kjj_010101@naver.com>
2024-03-08 15:50:39 +0900

    🔨 fix: dto 수정

--- src/clothes/dto/cloth-response.dto.ts
+++ src/clothes/dto/cloth-response.dto.ts
@@ -1,9 +1,21 @@
+import { ClothEntity } from './../../entities/clothes.entity';
+import { ClothCoatEntity } from 'src/entities/clothesCoat.entity';
+import { ClothBottomEntity } from './../../entities/clothesBottom.entity';
+import { ClothTopEntity } from './../../entities/clothesTop.entity';
+import { ClothAccessoryEntity } from 'src/entities/clothesAccessory.entity';
+
 export class ClothesTopDto {
   readonly id: number;
 
   readonly type: string;
 
   readonly imageUrl: string;
+
+  constructor(clothTopEntity: ClothTopEntity) {
+    this.id = clothTopEntity.id;
+    this.type = clothTopEntity.type;
+    this.imageUrl = clothTopEntity.image;
+  }
 }
 
 export class ClothesBottomDto {
@@ -12,6 +24,12 @@ export class ClothesBottomDto {
   readonly type: string;
 
   readonly imageUrl: string;
+
+  constructor(clothBottomEntity: ClothBottomEntity) {
+    this.id = clothBottomEntity.id;
+    this.type = clothBottomEntity.type;
+    this.imageUrl = clothBottomEntity.image;
+  }
 }
 
 export class ClothesCoatDto {
@@ -20,6 +38,12 @@ export class ClothesCoatDto {
   readonly type: string;
 
   readonly imageUrl: string;
+
+  constructor(clothCoatEntity: ClothCoatEntity) {
+    this.id = clothCoatEntity.id;
+    this.type = clothCoatEntity.type;
+    this.imageUrl = clothCoatEntity.image;
+  }
 }
 
 export class ClothesAccessoryDto {
@@ -28,6 +52,12 @@ export class ClothesAccessoryDto {
   readonly type: string;
 
   readonly imageUrl: string;
+
+  constructor(clothAccessoryntity: ClothAccessoryEntity) {
+    this.id = clothAccessoryntity.id;
+    this.type = clothAccessoryntity.type;
+    this.imageUrl = clothAccessoryntity.image;
+  }
 }
 
 export class ClothesDto {
@@ -40,6 +70,16 @@ export class ClothesDto {
   readonly clothesCoatId: ClothesCoatDto;
 
   readonly clothesAccessoryId: ClothesAccessoryDto;
+
+  constructor(clothEntity: ClothEntity) {
+    this.id = clothEntity.id;
+    this.clothesTopId = new ClothesTopDto(clothEntity.clothesTopId);
+    this.clothesBottomId = new ClothesBottomDto(clothEntity.clothesBottomId);
+    this.clothesCoatId = new ClothesCoatDto(clothEntity.clothesCoatId);
+    this.clothesAccessoryId = clothEntity.clothesAccessoryId
+      ? new ClothesAccessoryDto(clothEntity.clothesAccessoryId)
+      : null;
+  }
 }
 
 export class ClothesResponseDto {
